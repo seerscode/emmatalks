2702fa040b6506fdef482420de9e4e89312a8b47
Marcio <marcio@pense.dev>
2025-02-28 12:08:40 +0000

    slower

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function speak(word) {
     // Optional: Set properties for the utterance
     utterance.lang = 'en-US'; // Set the language
     utterance.pitch = 2; // Set the pitch (0 to 2)
-    utterance.rate = 0.3; // Set the rate to 0.7 for slower speech
+    utterance.rate = 0.5; // Set the rate to 0.7 for slower speech
     utterance.volume = 1; // Set the volume (0 to 1)
 
     window.speechSynthesis.speak(utterance);
