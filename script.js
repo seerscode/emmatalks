f87bec4091e582d125bea89d549afa0a95d4f5e0
Marcio <marcio@pense.dev>
2025-02-28 11:40:45 +0000

    edit utterance

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function speak(word) {
     const utterance = new SpeechSynthesisUtterance(word);
+    // Optional: Set properties for the utterance
+    utterance.lang = 'en-US'; // Set the language
+    utterance.pitch = 1; // Set the pitch (0 to 2)
+    utterance.rate = 0.7; // Set the rate to 0.7 for slower speech
+    utterance.volume = 1; // Set the volume (0 to 1)
+    
     window.speechSynthesis.speak(utterance);
 }
